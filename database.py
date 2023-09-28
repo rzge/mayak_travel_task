be4f17f99f1c409878d50e91a09f67d012db0485
Владислав Труханов <trukhanov.vladislav@gmail.com>
2023-09-28 12:28:52 +0300

    добавлена асинхронная функция для сохранения содержимого в БД

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import sqlite3
 
 # Устанавливаем соединение с базой данных
+import pandas as pd
+
 connection = sqlite3.connect('product_info.db')
 cursor = connection.cursor()
 
@@ -17,3 +19,20 @@ price REAL
 # Сохраняем изменения и закрываем соединение
 connection.commit()
 connection.close()
+
+# слой вызова асинхронных функций, работающие с асинхронным драйвером БД
+import aiosqlite
+
+
+async def save_to_db(excel_info: pd.DataFrame):
+    print('подключение к бд')
+    excel_info = excel_info.reset_index()
+    info_to_db = []
+    for index, row in excel_info.iterrows():
+        info_to_db.append((row['Товар'], row['Сайт'], row['Цена']))
+    print(info_to_db)
+    async with aiosqlite.connect('product_info.db') as db:
+        await db.executemany('INSERT INTO Информация (name, site, price) VALUES (?, ?, ?)',
+                             info_to_db)
+        await db.commit()
+        print('Коммитим изменения')
